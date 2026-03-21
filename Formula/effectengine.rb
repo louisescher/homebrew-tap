3018c3c0394fe9a1d112af21a63a9e2c3845bdd5
Louis Escher <66965600+louisescher@users.noreply.github.com>
2026-03-21 22:07:13 +0100

    Add Effectengine class with placeholder details

---
+++ Formula/effectengine.rb
@@ -0,0 +1,7 @@
+class Effectengine < Formula
+  desc "placeholder"
+  homepage "https://github.com/louisescher/effectengine"
+  url "https://github.com/louisescher/effectengine"
+  sha256 "0" * 64
+  def install; end
+end
